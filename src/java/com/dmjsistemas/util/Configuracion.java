644f78f2097974956863a92a1ef5a75c08fde4db
dmjm1720 <mario.arias@dmjm-sistemas.com>
2024-07-08 11:21:13 -0600

    Cambios pendientes

--- src/java/com/dmjsistemas/util/Configuracion.java
+++ src/java/com/dmjsistemas/util/Configuracion.java
@@ -3,20 +3,20 @@ package com.dmjsistemas.util;
 public class Configuracion {
 
     //**Windows**//
-    private static final String conexDBSae = "jdbc:sqlserver://192.168.1.37\\SQLEXPRESS;databaseName=SAE80Empre01";
-    private static final String conexDBCoi = "jdbc:sqlserver://192.168.1.37\\SQLEXPRESS;databaseName=COI90EMPRE1";
+    private static final String conexDBSae = "jdbc:sqlserver://192.168.1.37\\SQLEXPRESS;databaseName=SAE90Empre01";
+    private static final String conexDBCoi = "jdbc:sqlserver://192.168.1.37\\SQLEXPRESS;databaseName=COI10Empre1";
     private static final String conexUser = "sa";
     private static final String conexPwd = "Aspel**2013";
    
-//    private static final String conexDBSae = "jdbc:sqlserver://localhost;databaseName=SAE80Empre01";
-//    private static final String conexDBCoi = "jdbc:sqlserver://localhost;databaseName=COI90EMPRE1";
+//    private static final String conexDBSae = "jdbc:sqlserver://localhost;databaseName=SAE90Empre01";
+//    private static final String conexDBCoi = "jdbc:sqlserver://localhost;databaseName=COI10EMPRE1";
 //    private static final String conexUser = "sa";
 //    private static final String conexPwd = "Pr4xi5A5a*";
 //   
     //**Linux**//
 
-
-    public static String getConexDBSae() {
+         
+    public static String getConexDBSae() {            
         return conexDBSae;
     }
 
